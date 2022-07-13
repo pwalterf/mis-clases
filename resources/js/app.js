99ad8c5d9fc74f6672b6f3b4775dac18cec18e9e
pwalterf <pintowalterfabian@gmail.com>
2022-07-12 22:13:05 -0300

    Migrating to Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,17 @@
 import './bootstrap';
+import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp, usePage } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
         const VueApp = createApp({ render: () => h(app, props) });
 
@@ -39,6 +42,7 @@ createInertiaApp({
         }
 
         VueApp.use(plugin)
+            .use(ZiggyVue, Ziggy)
             .mixin({ methods: { route } })
             .mount(el);
     },
