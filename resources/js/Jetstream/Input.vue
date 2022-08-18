5f49e14956a4b4caa73cc3d56b61cd51f79d8b69
pwalterf <pintowalterfabian@gmail.com>
2022-08-18 18:09:04 -0300

    Add type in Input Component

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -2,6 +2,10 @@
 import { onMounted, ref } from 'vue';
 
 defineProps({
+    type: {
+        type: String,
+        default: 'text',
+    },
     modelValue: String,
     error: [String, Array],
 });
@@ -22,6 +26,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
+        :type="type"
         class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
         :class="{'border-red-500 focus:border-red-500 focus:ring-red-500': error}"
         :value="modelValue"
